235d476d2e176a82a7a11ccac557141cce75f187
ueshita <nalto32@gmail.com>
2022-01-05 01:59:48 +0900

    Change symbol prefix

--- Dev/Cpp/src/GDLibrary.cpp
+++ Dev/Cpp/src/GDLibrary.cpp
@@ -8,17 +8,17 @@
 
 using namespace godot;
 
-extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *option)
+extern "C" void GDN_EXPORT EffekseerGodot_gdnative_init(godot_gdnative_init_options *option)
 {
 	Godot::gdnative_init(option);
 }
 
-extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *option)
+extern "C" void GDN_EXPORT EffekseerGodot_gdnative_terminate(godot_gdnative_terminate_options *option)
 {
 	Godot::gdnative_terminate(option);
 }
 
-extern "C" void GDN_EXPORT godot_nativescript_init(void *handle)
+extern "C" void GDN_EXPORT EffekseerGodot_nativescript_init(void *handle)
 {
 	Godot::nativescript_init(handle);
 
