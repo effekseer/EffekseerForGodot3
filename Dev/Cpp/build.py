1e47333ed64cf726629b7d71a90a6ea1fd11943a
durswd <swda.durl@gmail.com>
2022-01-23 11:24:56 +0900

    Fix a bug where EffekseerForGodot cannot compile along documents

--- Dev/Cpp/build.py
+++ Dev/Cpp/build.py
@@ -17,11 +17,13 @@ def replace_word(file_name, target_str, replace_str):
 def import_generate_bindings():
     binding_generator = __import__("godot-cpp.binding_generator").binding_generator
     cwd = os.getcwd()
-    os.chdir(os.path.join(cwd, "godot-cpp"))
+    os.chdir(os.path.join(os.path.dirname(__file__), "godot-cpp"))
     binding_generator.generate_bindings("godot-headers/api.json", False)
     os.chdir(cwd)
 
 import_generate_bindings()
+
+os.chdir(os.path.dirname(__file__))
 
 job_opt = " -j" + str(multiprocessing.cpu_count())
 
